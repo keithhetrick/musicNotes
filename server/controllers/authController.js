30c961e579fd70ba60e5a5319bb055dc9f47481d
Keith Hetrick <keithjhetrick@gmail.com>
2023-02-05 14:31:55 -0600

    deployment prep

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -44,7 +44,8 @@ const login = asyncHandler(async (req, res) => {
   res.cookie("jwt", refreshToken, {
     httpOnly: true, // accessible only by web server
     secure: false, // https
-    sameSite: "None", // cross-site cookie
+    // enable sameSite true for production
+    sameSite: "Lax", // "None", // "Lax", // "Strict
     maxAge: 7 * 24 * 60 * 60 * 1000, // cookie expiry: set to match rT
   });
 
@@ -96,7 +97,7 @@ const refresh = (req, res) => {
 const logout = (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(204); // No content
-  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: false });
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "Lax", secure: false });
   res.json({ message: "Cookie cleared" });
 };
 
