22260483b8eb8c39c928eec4e6f1876235d3d709
Keith Hetrick <keithjhetrick@gmail.com>
2023-02-04 18:30:36 -0600

    route cleanup

--- server/server.js
+++ server/server.js
@@ -29,24 +29,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-// Use relative path to views folder
-// app.use("/", express.static(path.join(__dirname, "public")));
-
-app.use("/", require("./routes/root"));
-app.use("/auth", require("./routes/authRoutes.js"));
-app.use("/users", require("./routes/userRoutes.js"));
-app.use("/notes", require("./routes/noteRoute.js"));
-
-// app.all("*", (req, res) => {
-//   res.status(404);
-//   if (req.accepts("html")) {
-//     res.sendFile(path.join(__dirname, "views", "404.html"));
-//   } else if (req.accepts("json")) {
-//     res.json({ message: "404 Not Found" });
-//   } else {
-//     res.type("txt").send("404 Not Found");
-//   }
-// });
+app.use("/api/", require("./routes/root"));
+app.use("/api/auth", require("./routes/authRoutes.js"));
+app.use("/api/users", require("./routes/userRoutes.js"));
+app.use("/api/notes", require("./routes/noteRoute.js"));
 
 // Error handler
 app.use(errorHandler);
